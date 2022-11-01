7dac31a28baae71b574fe633b32e261d8995a02e
Alok <alokrkmv12@gmail.com>
2022-11-01 02:30:23 -0400

    Added a skeltal project for Elena

---
+++ src/server/map_generator/generate_map.py
@@ -0,0 +1,23 @@
+class GenerateMap():
+    
+    def __init__(self):
+        pass
+
+    # This method would find town, state and lat-long info for a particular location
+    # Returns a dictionary as params with lat, long, town and state as key
+    # We will be using geopy for finding lat, long of a particular location
+    def getLocationInfo(self, locationm : str): -> dict:
+        pass
+    
+    # This method generates map of the city and state with elevation info attached to each node
+    # For getting the elevation info we will be using elevation API of Google
+    # For generating the map we wil be using osmnx map generator
+    def generateMap(self,city, state):
+        pass
+
+
+
+
+
+
+    
---
+++ src/server/path_finder/djikistra_path_finder.py
@@ -0,0 +1,11 @@
+# This class implements a modified version of Djikistra shortest path
+# algorithm to find a suitable path satisfiying percetange path length and 
+# elevation gain requirement of the client
+class PathFinder():
+    def __init__(self):
+        pass
+    # This function takes a graph(map of the city), start_node, end_node, percentage path length, 
+    # max or min elevation gain and returns path(an array of nodes from source to destination)
+    # elevation gain, path length
+    def get_path(self, graph,start, end, percentage_length, min_max_gain):
+        pass
