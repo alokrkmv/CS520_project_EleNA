d2e2d82298ba218f2988def22d15641e4fd39819
Juie Darwade <32596421+juie-darwade@users.noreply.github.com>
2022-11-03 15:25:32 -0400

    Update generate_map.py

--- src/main/server/map_generator/generate_map.py
+++ src/main/server/map_generator/generate_map.py
@@ -6,13 +6,13 @@ class GenerateMap():
     # This method would find town, state and lat-long info for a particular location
     # Returns a dictionary as params with lat, long, town and state as key
     # We will be using geopy for finding lat, long of a particular location
-    def getLocationInfo(self, locationm : str): -> dict:
+    def getLocationInfo(self, location : str): -> dict:
         pass
     
     # This method generates map of the city and state with elevation info attached to each node
     # For getting the elevation info we will be using elevation API of Google
     # For generating the map we wil be using osmnx map generator
-    def generateMap(self,city, state):
+    def generateMap(self, city, state):
         pass
 
 
