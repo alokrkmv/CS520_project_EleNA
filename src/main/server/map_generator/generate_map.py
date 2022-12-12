ad60af5ca7d6b2e4f129464d14cfc110b4ae6d49
arpita <arpitasingh@umass.edu>
2022-12-12 06:29:15 -0500

    embedding map, generating route on map ui, adding autocomplete

--- src/main/server/map_generator/generate_map.py
+++ src/main/server/map_generator/generate_map.py
@@ -72,7 +72,7 @@ class GenerateMap():
                 graph = pkl.load(open(self.file_path,"rb"))
                 try:
                     logging.info(f"Plotting the graph for city {city} state {state}")
-                    self.generateGraphPlot(graph)
+                    # self.generateGraphPlot(graph)
                 except Exception as e:
                     logging.warning(f"Graph generated successfuly however plotting failed!!! Aborting plot generation!!!")
                 finally:
@@ -104,7 +104,8 @@ class GenerateMap():
                 logging.warning(f"Caching the graph failed with error {e}!!")
             finally:
                 try:
-                    self.generateGraphPlot(graph)
+                    # self.generateGraphPlot(graph)
+                    pass
                 except Exception as e:
                     logging.warning(f"Graph generated successfuly however plotting failed!!! Aborting plot generation!!!")
                 finally:
